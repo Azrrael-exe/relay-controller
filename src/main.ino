8d06db626ec189fd089639e276f5b40635df918d
Azrrael <vcamargo.e@gmail.com>
2018-05-08 02:33:35 -0500

    Self Modbus

--- src/main.ino
+++ src/main.ino
@@ -6,18 +6,16 @@
 #include <LCD.h>
 #include <LiquidCrystal_I2C.h>
 
-#include <ModbusMaster.h>
 #include <relay.h>
 
 #define N 8
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
-ModbusMaster node;
+Relay relays[N];
 
 long wd;
 uint8_t action = NON_ACTION;
-bool state = 0;
 
 void setup() {
   // --- Screen Init ---
@@ -28,75 +26,68 @@ void setup() {
 
   // --- Modbus Init ---
   Serial.begin(9600);
-  node.begin(1, Serial);
+  for(int i = 0; i<N ; i++){
+    relays[i] = Relay(i);
+  }
 }
+
+bool status = true;
 
 void loop() {
 
   char key = keypad.getKey();
 
-  if(key == '1'){
+  if(isDigit(key)){
+    uint8_t index = String(key).toInt();
+    uint8_t buf[8];
+    relays[index].getRelay(Serial, buf);
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("Set Adr 1");
-    lcd.setCursor(0, 1);
-    uint8_t frame[8] = {0x00, 0x06, 0x40, 0x00, 0x00, 0x01, 0x5c, 0x1b};
-    for(int i=0; i<8; i++){
-      Serial.write(frame[i]);
-    }
-    delay(300);
-    while(Serial.available()){
-      lcd.print(Serial.read(), HEX);
+    for(int i=0;i<6;i++){
+      lcd.print(buf[i], HEX);
       lcd.print(" ");
     }
   }
-  if(key == '2'){
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Read Relay");
-    lcd.setCursor(0, 1);
-    uint8_t frame[8] = {0x00, 0x03, 0x40, 0x00, 0x00, 0x01, 0x90, 0x1b};
-    for(int i=0; i<8; i++){
-      Serial.write(frame[i]);
-    }
-    delay(300);
-    while(Serial.available()){
-      lcd.print(Serial.read(), HEX);
-      lcd.print(" ");
-    }
-  }
-
-  if(key == '4'){
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Read Relay");
-    lcd.setCursor(0, 1);
-    uint8_t frame[8] = {0x01, 0x05, 0x00, 0x01, 0x01, 0x00, 0x9d, 0x9a};
-    for(int i=0; i<8; i++){
-      Serial.write(frame[i]);
-    }
-    delay(300);
-    while(Serial.available()){
-      lcd.print(Serial.read(), HEX);
-      lcd.print(" ");
-    }
-  }
-
-
 
   if(key == '#'){
     lcd.clear();
     lcd.setCursor(0,0);
     lcd.print("Write!: ");
-    uint8_t result = node.writeSingleCoil(0x0002, state);
-    state = !state;
-    lcd.print(result);
   }
 
   if(key == '*'){
+
+    while (Serial.available()) {
+      Serial.read();
+    }
+
+    uint8_t frame[8] = {0x01, 0x01, 0x00, 0x01, 0x00, 0x04, 0x00, 0x00};
+    uint16_t crc = 0xFFFF;
+    for (int i = 0; i < 6; i++) {
+      crc ^= (uint16_t)frame[i];
+      for (int i = 8; i != 0; i--) {    // Loop over each bit
+        if ((crc & 0x0001) != 0) {      // If the LSB is set
+          crc >>= 1;                    // Shift right and XOR 0xA001
+          crc ^= 0xA001;
+        }
+        else                            // Else LSB is not set
+         crc >>= 1;                    // Just shift right
+       }
+     }
+     // Note, this number has low and high bytes swapped, so use it accordingly (or swap bytes)
+    frame[6] = crc & 0x00FF;
+    frame[7] = crc >> 8;
+    for(int i=0; i<8; i++){
+      Serial.write(frame[i]);
+    }
+    delay(1000);
     lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Read!");
+    lcd.setCursor(0, 1);
+    while (Serial.available()) {
+      lcd.print(Serial.read(), HEX);
+      lcd.print(" ");
+    }
+
   }
 
 }
--- src/relay.h
+++ src/relay.h
@@ -11,10 +11,114 @@
 class Relay {
 public:
   Relay();
-  bool getState();
-  bool setState();
+  Relay(uint8_t i);
+  bool setRelay(Stream& uart, bool state);
+  bool getRelay(Stream& uart, uint8_t (&arr)[8]);
+  bool swapRelay(Stream& uart);
 private:
+  uint8_t index;
   bool state;
 };
+
+Relay::Relay(){
+  ;
+}
+
+Relay::Relay(uint8_t i){
+  index = i;
+}
+
+bool Relay::setRelay(Stream& uart, bool s){
+
+  while (uart.available()) {
+    uart.read();
+  }
+
+  uint8_t frame[8] = {0x01, 0x05, 0x00, index, s, 0x00, 0x00, 0x00};
+  uint16_t crc = 0xFFFF;
+  for (int i = 0; i < 6; i++) {
+    crc ^= (uint16_t)frame[i];
+    for (int i = 8; i != 0; i--) {    // Loop over each bit
+      if ((crc & 0x0001) != 0) {      // If the LSB is set
+        crc >>= 1;                    // Shift right and XOR 0xA001
+        crc ^= 0xA001;
+      }
+      else                            // Else LSB is not set
+       crc >>= 1;                    // Just shift right
+     }
+   }
+   // Note, this number has low and high bytes swapped, so use it accordingly (or swap bytes)
+  frame[6] = crc & 0x00FF;
+  frame[7] = crc >> 8;
+  for(int i=0; i<8; i++){
+    uart.write(frame[i]);
+  }
+  state = s;
+  return state;
+}
+
+bool Relay::getRelay(Stream& uart, uint8_t (&arr)[8]){
+
+  while (uart.available()) {
+    uart.read();
+  }
+
+  uint8_t frame[8] = {0x01, 0x01, 0x00, index, 0x00, 0x04, 0x00, 0x00};
+  uint16_t crc = 0xFFFF;
+  for (int i = 0; i < 6; i++) {
+    crc ^= (uint16_t)frame[i];
+    for (int i = 8; i != 0; i--) {    // Loop over each bit
+      if ((crc & 0x0001) != 0) {      // If the LSB is set
+        crc >>= 1;                    // Shift right and XOR 0xA001
+        crc ^= 0xA001;
+      }
+      else                            // Else LSB is not set
+       crc >>= 1;                    // Just shift right
+     }
+   }
+   // Note, this number has low and high bytes swapped, so use it accordingly (or swap bytes)
+  frame[6] = crc & 0x00FF;
+  frame[7] = crc >> 8;
+  for(int i=0; i<8; i++){
+    uart.write(frame[i]);
+  }
+  delay(100);
+  for(int i = 0; i<6; i++){
+    arr[i] = uart.read();
+  }
+  state = bool(arr[3]);
+  return state;
+}
+
+bool Relay::swapRelay(Stream& uart){
+
+  while (uart.available()) {
+    uart.read();
+  }
+
+  state = !state;
+  uint8_t frame[8] = {0x01, 0x05, 0x00, index, state, 0x00, 0x00, 0x00};
+  uint16_t crc = 0xFFFF;
+  for (int i = 0; i < 6; i++) {
+    crc ^= (uint16_t)frame[i];
+    for (int i = 8; i != 0; i--) {    // Loop over each bit
+      if ((crc & 0x0001) != 0) {      // If the LSB is set
+        crc >>= 1;                    // Shift right and XOR 0xA001
+        crc ^= 0xA001;
+      }
+      else                            // Else LSB is not set
+       crc >>= 1;                    // Just shift right
+     }
+   }
+
+   // Note, this number has low and high bytes swapped, so use it accordingly (or swap bytes)
+  frame[6] = crc & 0x00FF;
+  frame[7] = crc >> 8;
+  for(int i=0; i<8; i++){
+    uart.write(frame[i]);
+  }
+  return state;
+}
+
 
 #endif
